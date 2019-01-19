4e8d74f19edcef67af00c8d1d21b7b17fcd196d2
Martin Polak <nigol@nigol.cz>
2019-01-19 08:43:53 +0000

    Displaying of articles, structure refactoring.

---
+++ src/main/java/cz/nigol/obec/beans/ArticleBean.java
@@ -0,0 +1,35 @@
+package cz.nigol.obec.beans;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import cz.nigol.obec.entities.Article;
+import cz.nigol.obec.services.ArticleService;
+
+@Named
+@RequestScoped
+public class ArticleBean {
+    @Inject
+    private ArticleService articleService;
+    private Article article;
+
+    public void loadArticle(String id) {
+	article = articleService.getArticleById(id);
+	article = articleService.loadArticleBody(article);
+    }
+
+    /**
+     * @return the article
+     */
+    public Article getArticle() {
+	return article;
+    }
+
+    /**
+     * @param article the article to set
+     */
+    public void setArticle(Article article) {
+	this.article = article;
+    }
+}
