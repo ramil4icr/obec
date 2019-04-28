f567a6d0cfdab48ba448e93de29cfeb3880f452a
Martin Polak <nigol@nigol.cz>
2019-04-28 06:41:02 +0000

    /obecni-urad/uredni-deska/prehled.jsf - třídění upravit třídění tabulky.

--- src/main/java/cz/nigol/obec/entities/DeskItem.java
+++ src/main/java/cz/nigol/obec/entities/DeskItem.java
@@ -21,9 +21,9 @@ import javax.persistence.TemporalType;
 @NamedQuery(name=DeskItem.GET_ALL,
 query="SELECT d FROM DeskItem d ORDER BY d.activeFrom DESC"),
     @NamedQuery(name=DeskItem.GET_ACTIVE_TO_DATE,
-    query="SELECT d FROM DeskItem d WHERE (:date BETWEEN d.activeFrom AND d.activeTo) OR (:date >= d.activeFrom AND d.activeTo IS NULL) ORDER BY d.activeFrom"),
+    query="SELECT d FROM DeskItem d WHERE (:date BETWEEN d.activeFrom AND d.activeTo) OR (:date >= d.activeFrom AND d.activeTo IS NULL) ORDER BY d.activeFrom DESC"),
     @NamedQuery(name=DeskItem.GET_VALID_TO_DATE,
-    query="SELECT d FROM DeskItem d WHERE :date >= d.activeFrom ORDER BY d.activeFrom"),
+    query="SELECT d FROM DeskItem d WHERE :date >= d.activeFrom ORDER BY d.activeFrom DESC"),
 })
 @Entity
 @Table(name="OB_DESK_ITEM")
