167e1f26ca9784a31a19f29e6fa15921cd0d4f61
Martin Polak <nigol@nigol.cz>
2020-01-19 13:10:28 +0000

    Poll voting WIP.

---
+++ src/main/java/cz/nigol/obec/beans/PollBean.java
@@ -0,0 +1,62 @@
+package cz.nigol.obec.beans;
+
+import java.io.Serializable;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import cz.nigol.obec.entities.Poll;
+import cz.nigol.obec.entities.PollQuestion;
+import cz.nigol.obec.services.PollService;
+
+@Named
+@ViewScoped
+public class PollBean implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Inject
+    private PollService pollService;
+    @Inject
+    private FacesContext facesContext;
+    private Poll poll;
+    private long id;
+
+    public void onLoad() {
+        poll = pollService.findById(id);
+    }
+
+    public void vote(PollQuestion question) {
+        facesContext.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "", 
+                    "Děkujeme za Váš hlas pro volbu '" + question.getQuestion() + "'."));
+    }
+
+    /**
+     * @return the poll
+     */
+    public Poll getPoll() {
+        return poll;
+    }
+
+    /**
+     * @param poll the poll to set
+     */
+    public void setPoll(Poll poll) {
+        this.poll = poll;
+    }
+
+    /**
+     * @return the id
+     */
+    public long getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(long id) {
+        this.id = id;
+    }
+}
