33696b5dee0af2dea4ac08bc22a0c3ba591c235f
Martin Polak <nigol@nigol.cz>
2020-04-14 13:12:59 +0000

    WaterSpending bean.

---
+++ src/main/java/cz/nigol/obec/beans/WaterSpendingBean.java
@@ -0,0 +1,41 @@
+package cz.nigol.obec.beans;
+
+import java.io.*;
+import java.util.*;
+import javax.inject.*;
+import javax.faces.view.*;
+import javax.faces.context.FacesContext;
+import javax.faces.application.FacesMessage;
+import javax.annotation.*;
+import cz.nigol.obec.entities.*;
+import cz.nigol.obec.services.*;
+
+@Named
+@ViewScoped
+public class WaterSpendingBean implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Inject
+    private WaterSpendingService waterSpendingService;
+    @Inject
+    private FacesContext facesContext;
+    private WaterSpending waterSpending;
+
+    @PostConstruct
+    public void init() {
+        waterSpending = new WaterSpending();
+    }
+
+    public void save() {
+        waterSpendingService.saveWaterSpending(waterSpending);
+        init();
+        facesContext.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_INFO, "", "Vaše nabídka byla uložena. Děkujeme!"));
+    }
+
+    public WaterSpending getWaterSpending() {
+        return waterSpending;
+    }
+
+    public void setWaterSpending(WaterSpending waterSpending) {
+        this.waterSpending = waterSpending;
+    }
+}
