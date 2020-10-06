91743db43580be0637d69f846b95e4ea3f0fe949
Martin Polak <nigol@nigol.cz>
2020-10-06 11:32:08 +0000

    Password reset WIP.

---
+++ src/main/java/cz/nigol/obec/beans/PasswordResetBean.java
@@ -0,0 +1,56 @@
+package cz.nigol.obec.beans;
+
+import java.io.*;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+import javax.faces.view.ViewScoped;
+import javax.inject.*;
+
+import cz.nigol.obec.entities.*;
+import cz.nigol.obec.qualifiers.*;
+import cz.nigol.obec.services.*;
+
+@Named
+@ViewScoped
+public class PasswordResetBean implements Serializable {
+    private static final long serialVersionUID = 5058918096082190355L;
+    @Inject
+    @CurrentSettings
+    private Settings settings;
+    @Inject
+    private FacesContext facesContext;
+    @Inject
+    private UserService userService;
+    private String email;
+    private boolean sent;
+
+    @PostConstruct
+    public void init() {
+    }
+
+    public void onLoad() throws IOException {
+    }
+
+    public void send() {
+        sent = true;
+    }
+
+    public boolean isSent() {
+        return sent;
+    }
+
+    public void setSent(boolean sent) {
+        this.sent = sent;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
